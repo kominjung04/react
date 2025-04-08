b1bbe24039bfd2fe992c55fbf4777b09df37ffa9
kominjung04 <heppy8888@naver.com>
2025-04-08 19:34:49 +0900

    perf:되돌리기 기능 추가

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -5,10 +5,7 @@ function Square({value,onSquareClick}){
   return <button className='square' onClick={onSquareClick}>{value}</button>
 }
 
-export default function Board() {
-  const [xIsNext,setIsNext] = useState(true);
-  const [squares,setSquares] = useState(Array(9).fill(null));
-
+function Board({xIsNext,squares,onPlay}) {
   function handleClick(i){
     if(squares[i] || calculateWinner(squares)) {
       return;
@@ -16,12 +13,10 @@ export default function Board() {
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
-    }
-    else {
+    }else {
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
   
   const winner = calculateWinner(squares);
@@ -52,6 +47,50 @@ export default function Board() {
     </div>
     </>
   )
+}
+
+export default function Game(){
+  const [xIsNext,setIsNext] = useState(true);
+  const [history,setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove,setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0,currentMove + 1) ,nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length -1);
+    setIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 == 0);
+  }
+
+  const moves = history.map((squares,move) => {
+    let description;
+    if(move > 0) {
+    description = 'Go to move #' + move;
+  }else{
+    description = "Go to game start";
+  }
+  return(
+    <li key={move}>
+      <button onClick={() => jumpTo(move)}>{description}</button>
+    </li>
+  );
+});
+
+return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className='game-info'>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 }
 
 function calculateWinner(squares) {
